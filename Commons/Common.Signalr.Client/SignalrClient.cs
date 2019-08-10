f8e0fb604e83101cfc368134e9f5f7da5bb2fd37
swpuzhang <840757167@qq.com>
2019-08-11 00:08:36 +0800

    ii

--- Commons/Common.Signalr.Client/SignalrClient.cs
+++ Commons/Common.Signalr.Client/SignalrClient.cs
@@ -1,8 +1,10 @@
-﻿using System;
+﻿using Microsoft.AspNet.SignalR.Client;
+using System;
 
 namespace Common.Signalr.Client
 {
     public class SignalrClient
     {
+        private HubConnection _hubConnection;
     }
 }
